d927b66945fcbe9206c0c77e645eaecd2b29c1d6
Dari <darimarcaceres@outlook.com>
2025-08-31 02:50:12 -0400

    feat(routes): create routes
    * Add Home, Login, SignUp, Quiz, CreateQuiz, and AboutMe pages
    * Configure routes in app.jsx and main.jsx for new pages
    * Remove unused stock svg files

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,36 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from "react-router-dom";
 import './App.css'
+import AboutMe from "./routes/aboute-me/about-me";
+import CreateQuiz from "./routes/create-quiz/create-quiz";
+import Home from "./routes/home/home";
+import Login from "./routes/login/login";
+import Quiz from "./routes/quiz/quiz";
+import SignUp from "./routes/signup/signup";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>Hello world!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/create-quiz" element={<CreateQuiz/>}/>
+        <Route path="/quiz" element={<Quiz/>}/>
+        <Route path="/aboute-me" element={<AboutMe/>}/>
+    </Routes>
+  );
 }
 
 export default App

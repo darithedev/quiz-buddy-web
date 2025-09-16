1e2e07160323c80c7cbc3b4c0da3083ed077f83f
Dari Caceres <demzko@outlook.com>
2025-09-15 23:57:53 -0400

    feat(home): Fix responsiveness on Home page
    * Add '$nav-hamburger-color' variable to _variables.scss file
    * Fix div in home.jsx file by moving 'expand="md"' to Navbar
    * Update home.scss to add padding to navbar and remove border on hamberger dropdown when in mobile view
    * Minor update to signup.jsx file to correct routing to /login

--- front-end/src/routes/home/home.jsx
+++ front-end/src/routes/home/home.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import { Button, Navbar, Nav, Container } from "react-bootstrap";
 import "./home.scss";
 
+
 function Home() {
     
     return (
-        <div expand="md" className="home-wrapper">
-            <Navbar className="d-flex flex-column m-3">
+        <div className="home-wrapper">
+            <Navbar expand="md" className="d-flex flex-column m-3">
                 <Container>
-                    <Navbar.Brand  href="home" className="m-4 ms-5 title-link">QuizBuddy</Navbar.Brand>
+                    <Navbar.Brand href="home" className="m-4 ms-5 title-link">QuizBuddy</Navbar.Brand>
                     <Navbar.Toggle aria-controls="hamburger-menu" />
                     <Navbar.Collapse id="hamburger-menu" className="home-nav-bar">
                         <Nav className="m-4 ms-5">
-                            <Nav.Link href="/create-quiz" className="color-secondary">Create Quiz Page</Nav.Link>
-                            <Nav.Link href="/about" className="color-secondary">About Page</Nav.Link>
-                            <Nav.Link href="/quiz" className="color-secondary">Quiz Page</Nav.Link>
+                            <Nav.Link href="/create-quiz" className="color-secondary">Create a Quiz</Nav.Link>
+                            <Nav.Link href="/about" className="color-secondary">About</Nav.Link>
+                            <Nav.Link href="/quiz" className="color-secondary">Take Quiz</Nav.Link>
                             <Nav.Link href="/" className="color-secondary">Logout</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>

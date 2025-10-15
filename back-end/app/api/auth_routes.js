36a2fdd6e13a6f62803e747f60602064afcf3b61
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-10-14 20:46:02 -0400

    feat(backend): Updated auth_routes to include login route
    * POST Login in route added

--- back-end/app/api/auth_routes.js
+++ back-end/app/api/auth_routes.js
@@ -3,6 +3,29 @@ import { supabase } from "../supabase";
 
 export async function POST(request) {
     try {
+        const { userName, password } = await request.json()
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            userName: userName,
+            password: password
+        })
+
+        if (error) {
+            return NextResponse.json({error: error.message}, {status: 400})
+        }
+
+        return NextResponse.json ({
+            message: 'User logged in',
+            user: data.user
+        })
+
+    } catch (error) {
+        return NextResponse.json({error: 'User NOT logged in.'}, {status: 500})
+    }
+}
+
+export async function POST(request) {
+    try {
         const { fullName, email, userName, password } = await request.json()
 
         const { data, error } = await supabase.auth.signUp({

fe2aef4230749bc2cc1f80eecb501dc4af04da5a
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-10-21 19:06:51 -0400

    feat(signup): Update signup page for events and add .gitignore
    * Added code to handle user data for signup page
    * Added code to handle even changes
    * Added code to handle event submission
    * Added .gitignore to protect .env information

--- front-end/src/routes/signup/signup.jsx
+++ front-end/src/routes/signup/signup.jsx
@@ -3,6 +3,35 @@ import { Button, Card, Form } from "react-bootstrap";
 import "./signup.scss";
 
 function SignUp() {
+    const [userData, setUserData] = useState({
+        fullName: '',
+        email: '',
+        userName: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const URL = import.meta.env.API_URL
+            const api = await fetch(`${URL}/api/auth/signup`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(userData)
+            });
+
+        } catch (error) {
+            alert('ERROR! Account was not created. Please try again.')
+        }
+    }
 
     return (
         <div className="login-wrapper">
@@ -11,23 +40,50 @@ function SignUp() {
                     <div className="text-center color">
                         <Card.Title className="mb-4" as="h1">Create Your Account</Card.Title>
                     </div>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <div>
-                            <Form.Group className="mb-3" controlId="formName">
+                            <Form.Group className="mb-3">
                                 <Form.Label className="color">Full Name</Form.Label>
-                                <Form.Control className="text-box" required type="text" placeholder=" Jane Shmane" />
+                                <Form.Control
+                                    name="fullName"
+                                    value={userData.fullName}
+                                    onChange={handleChange}
+                                    className="text-box" 
+                                    required type="text" 
+                                    placeholder=" Jane Shmane" 
+                                />
                             </Form.Group>
-                            <Form.Group className="mb-3" controlId="Email">
+                            <Form.Group className="mb-3">
                                 <Form.Label className="color">Email</Form.Label>
-                                <Form.Control className="text-box" required type="email" placeholder=" example@mail.com" />
+                                <Form.Control
+                                    name="email"
+                                    value={userData.email}
+                                    onChange={handleChange}
+                                    className="text-box" 
+                                    required type="email" 
+                                    placeholder=" example@mail.com" 
+                                />
                             </Form.Group>
-                            <Form.Group className="mb-3" controlId="userName">
+                            <Form.Group className="mb-3">
                                 <Form.Label className="color">User Name</Form.Label>
-                                <Form.Control className="text-box" placeholder=" example123" />
+                                <Form.Control
+                                    name="userName"
+                                    value={userData.userName}
+                                    onChange={handleChange}
+                                    className="text-box" 
+                                    placeholder=" example123" 
+                                />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formPassword">
                                 <Form.Label className="color-sec">Password </Form.Label>
-                                <Form.Control className="text-box" required type="password" placeholder=" Password321" />
+                                <Form.Control
+                                    name="password"
+                                    value={userData.password}
+                                    onChange={handleChange}
+                                    className="text-box" 
+                                    required type="password" 
+                                    placeholder=" Password321" 
+                                />
                             </Form.Group>
                             <Form.Group className="mb-5 button-wrap">
                                 <Button href="/" type="submit" className="mt-3 w-100 button">

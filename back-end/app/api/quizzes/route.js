ce8c748d1a239fac22544440cc847b195b209ab8
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-10-22 01:39:02 -0400

    feat(backend): Add POST request to quizzes route and GET request to users group

--- back-end/app/api/quizzes/route.js
+++ back-end/app/api/quizzes/route.js
@@ -1,2 +1,28 @@
 import { NextResponse } from "next/server";
 import { supabase } from "../../supabase";
+
+export async function POST(request) {
+    try {
+        const { userId, title, questions, count, score } = await request.json()
+        const { data, error } = await supabase
+            .from('quizzes')
+            .insert({
+                user_id:userId,
+                quiz_title: title,
+                num_questions:count,
+                questions: questions,
+                score: score
+            })
+        
+        if (error) {
+            return NextResponse.json({error: error.message}, {status: 400})
+        }
+
+        return NextResponse.json ({
+            message: 'Quiz was created.',
+            quiz: data
+        })
+    } catch (error) {
+        return NextResponse.json({error: 'ERROR, quiz was not created! Try again.'}, {status: 500})
+    }
+}
--- back-end/app/api/users/route.js
+++ back-end/app/api/users/route.js
@@ -1,2 +1,25 @@
 import { NextResponse } from "next/server";
 import { supabase } from "../../supabase";
+
+export async function GET(request) {
+    try {
+        const { searchParms } = new URL(request.url);
+        const userId = searchParms.get('userId');
+
+        const { data, error} = await supabase
+            .from('users')
+            .select('*')
+            .eq('id', userId)
+
+        if (error) {
+            return NextResponse.json({error: error.message}, {status: 400})
+        }
+
+        return NextResponse.json ({
+            message: 'User was located',
+            user: data
+        })
+    } catch (error) {
+        return NextResponse.json({error: 'ERROR, User is not registed in database'}, {status: 500})
+    }
+}

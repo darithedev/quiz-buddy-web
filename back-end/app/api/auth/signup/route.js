6ba5ac571bad2d6610981371ba671adc114bd19a
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-10-27 14:16:29 -0700

    fix(backend): Fix typo and add frontend link to CORS headers
    * Corrected a minor typo in signup POST headers to 'Access'
    * Added front end URL link to headers instead of '*'

--- back-end/app/api/auth/signup/route.js
+++ back-end/app/api/auth/signup/route.js
@@ -5,7 +5,7 @@ export async function OPTIONS() {
     return new Response(null, {
         status: 200,
         headers: {
-            'Acecss-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': 'https://quiz-buddy-web.vercel.app/',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization'
         },
